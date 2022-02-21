f1348485f4e5d50846afc741cdb7e4e310743360
jessica <fuerte.jessica10@gmail.com>
2022-02-21 16:58:09 -0600

    did server auth

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,6 +4,11 @@
 
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router()
+const { 
+  checkUsernameExists,
+  checkPasswordLength,
+  checkUsernameFree
+} = require('./auth-middleware')
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
